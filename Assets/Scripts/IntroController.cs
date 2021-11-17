ad600f86870d0b7e13d3117c1c110dc67b3a0ab6
Malte <maltewollermann@yahoo.de>
2021-11-17 16:15:58 +0100

    Finished tutorial prompts. some environment tests

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using OVR;
 
 /// <summary>
@@ -11,12 +12,16 @@ public class IntroController : MonoBehaviour
     [SerializeField]
     private GameObject centerEye; /* The center eye object of the VR rig, to be used for height information */
 
+    // The following gameobjects are used to let the user enter their age, gender, and provide some instructions. The parents of the canvases and text objects are provided, to set them (and all their children)
+    // to active/inactive.
     [SerializeField]
-    private GameObject ageInputParent;
+    private GameObject ageInputParent; /* Parent of the numpad letting the user enter their age */
     [SerializeField]
-    private GameObject confirmBackParent;
+    private GameObject confirmBackParent; /* Parent of the buttons to continue or go back */
     [SerializeField]
-    private GameObject genderInputParent;
+    private GameObject genderInputParent; /* Parent of the gender selection for the user */
+    [SerializeField]
+    private GameObject instructions01Parent; /* Parent of the first set of instructions */
 
     [SerializeField]
     private Vector3 playerEyeHeight; /* The height of the player */
@@ -24,9 +29,8 @@ public class IntroController : MonoBehaviour
     [SerializeField]
     private float heightOffset;
 
-    private int introState = 0; /* The state the intro level is in, increases when the user presses the 'Confirm' button */
+    private int introState = 0; /* The state the introduction is in, increases when the user presses the 'Confirm' button and decreases if they press the 'Back' button */
 
-    // Start is called before the first frame update
     void Start()
     {
 
@@ -35,17 +39,18 @@ public class IntroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // If the height was not correct and the object is uncomfortable or unusable for the player, reset it by pressing the "A" or "X" button on the controller
-        if (OVRInput.Get(OVRInput.Button.One))
+        // If the height was not correct and the object heights are uncomfortable or unusable for the player, reset them by pressing the "A" or "X" button on the controllers
+        if (OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Three))
         {
             readHeightAndSetToObject(ageInputParent, heightOffset);
             readHeightAndSetToObject(confirmBackParent, heightOffset);
             readHeightAndSetToObject(genderInputParent, heightOffset);
+            readHeightAndSetToObject(instructions01Parent, heightOffset);
         }
     }
 
     /// <summary>
-    /// Reads out the height of the VR device on the players head and sets the gameobject (for example the age input object) to an according height
+    /// Reads out the height of the VR device on the players head and sets the gameobjects (for example the age input object) to an according height. This allows for users with different heights.
     /// </summary>
     /// <param name="gameObject">The GameObject which has its height changed</param>
     /// <param name="offset">An optional offset value to be added to the height</param>
@@ -57,29 +62,49 @@ public class IntroController : MonoBehaviour
                                                 gameObject.transform.position.z); /* Set the age input parent height accordingly */
     }
 
+    /// <summary>
+    /// Listener function for the confirm button. In the intro, we ask for age and gender, since the user can go back and forth, the introState variable keeps track of where they are.
+    /// For example: introState == 0 means start of the application and age prompt, introState == 1 is after the user pressed the 'Confirm' button once, displaying the gender prompt now.
+    /// </summary>
     public void confirmButtonPressed()
     {
-        if(introState == 0)
+        switch(introState)
         {
-            ageInputParent.SetActive(false);
-            genderInputParent.SetActive(true);
-            introState++;
+            case 0:
+                ageInputParent.SetActive(false);
+                genderInputParent.SetActive(true);
+                introState++;
+                break;
+            case 1:
+                genderInputParent.SetActive(false);
+                instructions01Parent.SetActive(true);
+                introState++;
+                break;
+            case 2:
+                SceneManager.LoadScene(1); /* Load the main scene */
+                break;
         }
     }
 
+    /// <summary>
+    /// Listener function for the back button, same functionality as the previous listener, but in the other 'direction'.
+    /// </summary>
     public void backButtonPressed()
     {
-        if(introState == 0)
+        switch(introState)
         {
-            // Do nothing
-        } else
-        {
-            if(introState == 1)
-            {
+            case 0:
+                break;
+            case 1:
                 genderInputParent.SetActive(false);
                 ageInputParent.SetActive(true);
                 introState--;
-            }
-        }        
+                break;
+            case 2:
+                instructions01Parent.SetActive(false);
+                genderInputParent.SetActive(true);
+                introState--;
+                break;
+        }
     }
 }
